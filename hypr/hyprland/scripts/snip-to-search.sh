3685fb5e78326aad66c5497e4ed29bfdd61eb110
kneerazzz <kneeraxzz@gmail.com>
2026-08-07 20:20:45 +0530

    new: snip-to-screen file

---
+++ hypr/hyprland/scripts/snip-to-search.sh
@@ -0,0 +1,4 @@
+grim -g "$(slurp)" /tmp/image.png
+imageLink=$(curl -sF files[]=@/tmp/image.png 'https://uguu.se/upload' | jq -r '.files[0].url')
+xdg-open "https://lens.google.com/uploadbyurl?url=${imageLink}"
+rm /tmp/image.png
